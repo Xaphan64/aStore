4da69917e41d28ac5ebd9cc0e74147659b4271f2
Xaphan64 <alex_daniel64@yahoo.com>
2024-04-27 17:05:31 +0300

    fixed no text in image carousel in main page

--- src/components/pages/MainPage/AdsContainer.jsx
+++ src/components/pages/MainPage/AdsContainer.jsx
@@ -1,5 +1,8 @@
 // ASSETS
 import { leftArrowIcon, rightArrowIcon, circleIcon, circleEmptyIcon } from "../../assets/MUI-icons";
+import slideOne from "../../assets/carousel/slideOne.webp";
+import slideTwo from "../../assets/carousel/slideTwo.png";
+import slideThree from "../../assets/carousel/slideThree.jpg";
 
 // STYLES
 
@@ -21,9 +24,10 @@ const ImageSlider = ({ slides }) => {
   // STATE CONSTANTS
   const [currentSlide, setCurrentSlide] = useState(0);
 
-  const slideBackground = {
-    backgroundImage: `url(${slides[currentSlide].url})`,
-  };
+  // const slideBackground = {
+  //   backgroundImage: `url(${slides[currentSlide].url})`,
+  // };
+
   // LIFE CYCLE
   useEffect(() => {
     const changeSlide = setInterval(() => {
@@ -63,53 +67,38 @@ const ImageSlider = ({ slides }) => {
       <div className="ads-carousel-arrow right" onClick={handleNextSlide}>
         {rightArrowIcon}
       </div>
-      <div className="ads-slide" style={slideBackground}>
-        {/* <span className="ads-text">
-          {currentSlide === 0 && (
-            <span>
-              This is a Ecommerce project made by Alexandru-Daniel Sarbu. This project is made in React using API and
-              Axios library.
-            </span>
-          )}
 
-          {currentSlide === 1 && (
-            <span>
-              In this project you can create, add, delete and edit a product only if you are an admin. If you are a
-              normal user you can add to favorites, add to card and buy one or multiple products.
-            </span>
-          )}
-
-          {currentSlide === 2 && (
-            <span>
-              Please note that if the products are not loading refresh the page some times, sometimes the API fetch
-              works very slowly.
-            </span>
-          )}
-        </span> */}
-      </div>
-
-      <span className="ads-text">
-        {currentSlide === 0 && (
+      {/* <div className="ads-slides"> */}
+      {currentSlide === 0 && (
+        <div className="ads-slide">
+          <img src={slideOne} alt="slideOne" />
           <span>
-            This is a Ecommerce project made by Alexandru-Daniel Sarbu. This project is made in React using API and
-            Axios library.
+            This is a Ecommerce project made by Alexandru-Daniel Sarbu. This project is made in React using Axios
+            library, fetching API from a JSON server.
           </span>
-        )}
+        </div>
+      )}
 
-        {currentSlide === 1 && (
+      {currentSlide === 1 && (
+        <div className="ads-slide">
+          <img src={slideTwo} alt="slideTwo" />
           <span>
-            In this project you can create, add, delete and edit a product only if you are an admin. If you are a normal
-            user you can add to favorites, add to card and buy one or multiple products.
+            As a user, in this project you can add products to favorite, add products to cart and buy multiple products.
+            As an admin you can even create, delete and edit products.
           </span>
-        )}
+        </div>
+      )}
 
-        {currentSlide === 2 && (
+      {currentSlide === 2 && (
+        <div className="ads-slide">
+          <img src={slideThree} alt="slideThree" />
           <span>
-            Please note that if the products are not loading refresh the page some times, sometimes the API fetch works
-            very slowly.
+            Due to the fact that the JSON server is deployed on render.com the API may fetch with a several minutes
+            delay for the first time.
           </span>
-        )}
-      </span>
+        </div>
+      )}
+      {/* </div> */}
 
       <div className="ads-dots-container">
         {currentSlide === 0 ? (
